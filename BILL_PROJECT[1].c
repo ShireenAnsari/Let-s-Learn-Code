ea60a74482733382df92a817b333de00b44f0493
Shireen57 <93506160+Shireen57@users.noreply.github.com>
2022-02-05 21:15:02 +0500

    initial Comit

---
+++ BILL_PROJECT[1].c
@@ -0,0 +1,570 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+void gotoxy(int x, int y)
+{
+	COORD c;
+	c.X=x+56;
+	c.Y=y+3;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
+int main()
+{
+	system("color B0");
+	int i,d,b,a,c,q,t,u,w,oop,_1,conu2,conu3,conu4,conu5,conu6,conu7,conu8,conu9,conu10,conu11,conu12,bill,cbill,conu,ext,PrRe,mf,unt,Prrre;
+	long y,x,id,ref,no;
+	float lod,lop,rconu,ce,serre,totall,Eldu,ptv,tax,sal,toll,tr,Arr,adj,taxonfp,tob,ed,fulpr;
+	float chg, surchage, calamt,netamt;
+	char pr,m,s,r,l,v,sak,metr[40];
+	char ch,nwb,kv,Ras,Bu,Mor,Rod,Sak,cit[30];
+	int i2;
+	printf("                                  HYDERABAD ELECTRIC SUPPLY COMPANY\n\t\t\t A customer friendly company\tELECTRICITY CONSUMER BILL\t\t www.hesco.govt.pk\n");
+	for(i2=0;i2<=165;i2++)
+	printf("\xCD");
+	printf("\n");
+	
+	for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,1);//1
+    	printf("%c",-60);
+	}
+	
+	printf("\n");
+	for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,3);
+    	printf("%c",-60);
+	}
+		
+	printf("\n");
+	for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,5);
+    	printf("%c",-60);
+	}
+	printf("\n");
+	for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,7);
+    	printf("%c",-60);
+	}
+		printf("\n");
+	for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,9);
+    	printf("%c",-60);
+	}
+		printf("\n");
+		for(i2=-56;i2<=110;i2++)
+    {
+    	gotoxy(i2,11);
+    	printf("%c",-60);
+	}
+	printf("\n");
+	for(i2=0;i2<=10;i2++)
+    {
+    	gotoxy(-35,i2);
+    	printf("%c",-77);
+	}
+	
+	for(i2=0;i2<=10;i2++)
+    {
+    	gotoxy(-15,i2);
+    	printf("%c",-77);
+	}
+	for(i2=0;i2<=10;i2++)
+    {
+    	gotoxy(5,i2);
+    	printf("%c",-77);
+	}
+	for(i2=0;i2<=30;i2++)
+    {
+    	gotoxy(25,i2);
+    	printf("%c",-77);
+	}
+    for(i2=0;i2<=8;i2++)
+    {
+    	gotoxy(44,i2);
+    	printf("%c",-77);
+	}
+	for(i2=0;i2<=2;i2++)
+    {
+    	gotoxy(60,i2);
+    	printf("%c",-77);
+	}
+	for(i2=10;i2<=30;i2++)
+    {
+    	gotoxy(35,i2);
+    	printf("%c",-77);
+	}
+	for(i2=10;i2<=30;i2++)
+    {
+    	gotoxy(48,i2);
+    	printf("%c",-77);
+	}
+	for(i2=10;i2<=30;i2++)
+    {
+    	gotoxy(61,i2);
+    	printf("%c",-77);
+	}
+	printf("\n");
+		for(i2=0;i2<=165;i2++)
+    {
+    	gotoxy(i2,-10);
+    	printf("%c",-60);
+	}
+	
+	for(i2=-58;i2<=22;i2++)
+    {
+    	gotoxy(i2,20);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=22;i2++)
+    {
+    	gotoxy(i2,22);
+    	printf("%c",-60);
+    	
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,33);
+    	printf("%c",-60);
+	}
+		for(i2=21;i2<=30;i2++)
+    {
+    	gotoxy(-42,i2);
+    	printf("%c",-77);
+	}
+	for(i2=21;i2<=30;i2++)
+    {
+    	gotoxy(-28,i2);
+    	printf("%c",-77);
+	}
+	for(i2=21;i2<=30;i2++)
+    {
+    	gotoxy(-13,i2);
+    	printf("%c",-77);
+	}
+	for(i2=21;i2<=30;i2++)
+    {
+    	gotoxy(-6,i2);
+    	printf("%c",-77);
+	}
+	for(i2=21;i2<=30;i2++)
+    {
+    	gotoxy(9,i2);
+    	printf("%c",-77);
+	}
+	
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,35);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,37);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,39);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,41);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,43);
+    	printf("%c",-60);
+	}	for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,45);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,47);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,49);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,51);
+    	printf("%c",-60);
+	}
+		for(i2=-58;i2<=111;i2++)
+    {
+    	gotoxy(i2,53);
+    	printf("%c",-60);
+	}
+		for(i2=-10;i2<=111;i2++)
+    {
+    	gotoxy(i2,55);
+    	printf("%c",-60);
+	}
+	for(i2=-10;i2<=111;i2++)
+    {
+    	gotoxy(i2,57);
+    	printf("%c",-60);
+	}
+	for(i2=34;i2<=53;i2++)
+    {
+    	gotoxy(-31,i2);
+    	printf("%c",-77);
+	}
+	for(i2=32;i2<=57;i2++)
+    {
+    	gotoxy(-11,i2);
+    	printf("%c",-77);
+	}
+	for(i2=34;i2<=57;i2++)
+    {
+    	gotoxy(9,i2);
+    	printf("%c",-77);
+	}
+	for(i2=31;i2<=58;i2++)
+    {
+    	gotoxy(25,i2);
+    	printf("%c",-77);
+	}
+	for(i2=34;i2<=58;i2++)
+    {
+    	gotoxy(50,i2);
+    	printf("%c",-77);
+	}
+	gotoxy(-55,0);
+	printf("connection date");
+	gotoxy(-10,0);
+	printf("Bill Month");
+	gotoxy(-30,0);
+	printf("connected load");
+	gotoxy(7,0);
+	printf("    ED@");
+	gotoxy(28,0);
+	printf("Reading date");
+	gotoxy(48,0);
+	printf("Issue date");
+	gotoxy(65,0);
+	printf("Due date");
+	gotoxy(-55,4);
+	printf("consumer ID");
+	gotoxy(-31,4);
+	printf(" TARIFF");
+	gotoxy(-9,4);
+	printf("Load");
+	gotoxy(8,4);
+	printf("Old Ac Number");
+	gotoxy(29,4);
+	printf("Division");
+	gotoxy(29,6);
+	printf("Sub Division");
+	gotoxy(29,8);
+	printf("Feeder Name");
+	gotoxy(-55,8);
+	printf("Reference no");
+	gotoxy(-31,8);
+	printf("Lock Age");
+	gotoxy(-9,8);
+	printf("No of Ac");
+	gotoxy(8,8);
+	printf("Unbilled Age");
+	gotoxy(-55,12);
+	printf("Name & Address");
+	gotoxy(27,10);
+	printf("Month");
+	gotoxy(38,10);
+	printf("Units");
+	gotoxy(51,10);
+	printf("Bill");
+	gotoxy(68,10);
+	printf("Payment");
+	gotoxy(27,13);
+	printf("Dec 19");
+	gotoxy(27,14);
+	printf("Jan 19");
+	gotoxy(27,15);
+	printf("Feb 19");
+	gotoxy(27,16);
+	printf("Mar 19");
+	gotoxy(27,17);
+	printf("Apr 19");
+	gotoxy(27,18);
+	printf("May 19");
+	gotoxy(27,19);
+	printf("Jun 19");
+	gotoxy(27,20);
+	printf("Jul 19");
+	gotoxy(27,21);
+	printf("Aug 19");
+	gotoxy(27,22);
+	printf("Sep 19");
+	gotoxy(27,23);
+	printf("Oct 19");
+	gotoxy(27,24);
+	printf("Nov 19");
+	gotoxy(-54,21);
+	printf("Metter No");
+	gotoxy(-38,21);
+	printf("Previous");
+	gotoxy(-23,21);
+	printf("Present");
+	gotoxy(-10,21);
+	printf("MF");
+	gotoxy(0,21);
+	printf("Units");
+	gotoxy(12,21);
+	printf("Status");
+	gotoxy(-55,32);
+	printf("HESCO CHARGES");
+	gotoxy(-9,32);
+	printf("GOVT. CHARGES");
+	gotoxy(28,32);
+	printf("Total charges");
+	gotoxy(-55,34);
+	printf("Unit consumed");
+	gotoxy(-55,36);
+	printf("Cost of Electricity");
+	gotoxy(-55,38);
+	printf("Metter Rent");
+	gotoxy(-55,40);
+	printf("Service Rent");
+	gotoxy(-55,42);
+	printf("Fuel Price adjustment");
+	gotoxy(-55,52);
+	printf("Total");
+	gotoxy(-8,34);
+	printf("Electricity duty");
+	gotoxy(-8,36);
+	printf("PTV FEE");
+	gotoxy(-8,38);
+	printf("GST");
+	gotoxy(-8,40);
+	printf("Income Tax");
+	gotoxy(-8,42);
+	printf("Extra TAX");
+	gotoxy(-8,44);
+	printf("Further TAX");
+	gotoxy(-8,46);
+	printf("N.J Surcharge");
+	gotoxy(-8,48);
+	printf("Sales Tax");
+	gotoxy(-8,50);
+	printf("FC Surcharge");
+	gotoxy(-8,52);
+	printf("TR Surcharge");
+	gotoxy(-8,54);
+	printf("TAXES ON FPA");
+	gotoxy(-8,56);
+	printf("Total");
+	gotoxy(28,34);
+	printf("ARREAR/AGE");
+	gotoxy(28,36);
+	printf("Current Bill");
+	gotoxy(28,38);
+	printf("BILL Adjustment");
+	gotoxy(28,40);
+	printf("Installment");
+	gotoxy(28,42);
+	printf("Subsidies");
+	gotoxy(28,44);
+	printf("Total FPA");
+	gotoxy(27,46);
+	printf("Payable within due date");
+	gotoxy(28,48);
+	printf("L.P Surcharge");
+	gotoxy(28,50);
+	printf("Payable after due date");
+	gotoxy(-55,2);
+	scanf("%d%s%ld",&d,&m,&y);
+	gotoxy(-30,2);
+	scanf("%f",&lod);
+	gotoxy(-9,2);
+	scanf("%d%s",&b,&s);
+	gotoxy(10,2);
+	scanf("%f%c",&ed,&pr);
+	gotoxy(28,2);
+	scanf("%d%s%ld",&a,&l,&c);
+	gotoxy(47,2);
+	scanf("%d%s%ld",&q,&r,&t);
+	gotoxy(66,2);
+	scanf("%d%s%ld",&u,&v,&w);
+	gotoxy(-55,6);
+	scanf("%d",&id);
+	gotoxy(-30,6);
+	scanf("%s",&ch);
+	gotoxy(-9,6);
+	scanf("%f",&lop);
+	gotoxy(10,6);
+	scanf("%ld%c",&x,&r);
+	gotoxy(59,4);
+	scanf("%s",&nwb);
+	gotoxy(59,6);
+	scanf("%s",&sak);
+	gotoxy(59,8);
+	scanf("%s%S",&kv,&cit);
+	gotoxy(-55,10);
+	scanf("%ld",&ref);
+	gotoxy(-9,10);
+	scanf("%d",&_1);
+	gotoxy(-55,15);
+	scanf("%s%s%s%s%s",&Ras,&Bu,&Mor,&Rod,&sak);
+	gotoxy(-54,23);
+	scanf("%s%ld",&metr,&no);
+	gotoxy(-38,23);
+	scanf("%d",&PrRe);
+	gotoxy(-27,23);
+	scanf("%d",&Prrre);
+	gotoxy(-9,23);
+	scanf("%d",&mf);
+	gotoxy(2,23);
+	scanf("%d",&unt);
+	gotoxy(37,13);
+	scanf("%d",&conu);
+	gotoxy(52,13);
+	bill=conu*8;
+	printf("%d",bill);
+	gotoxy(65,13);
+	printf("%d",bill);
+	gotoxy(37,14);
+	scanf("%d",&conu2);
+	gotoxy(52,14);
+	bill=conu2*8;
+	printf("%d",bill);
+	gotoxy(65,14);
+	printf("%d",bill);
+	gotoxy(37,15);
+	scanf("%d",&conu3);
+	gotoxy(52,15);
+	bill=conu3*8;
+	printf("%d",bill);
+	gotoxy(65,15);
+	printf("%d",bill);
+	gotoxy(37,16);
+	scanf("%d",&conu4);
+	gotoxy(52,16);
+	bill=conu4*8;
+	printf("%d",bill);
+	gotoxy(65,16);
+	printf("%d",bill);
+	gotoxy(37,17);
+	scanf("%d",&conu5);
+	gotoxy(52,17);
+	bill=conu5*8;
+	printf("%d",bill);
+	gotoxy(65,17);
+	printf("%d",bill);
+	gotoxy(37,18);
+	scanf("%d",&conu6);
+	gotoxy(52,18);
+	bill=conu6*8;
+	printf("%d",bill);
+	gotoxy(65,18);
+	printf("%d",bill);
+	gotoxy(37,19);
+	scanf("%d",&conu7);
+	gotoxy(52,19);
+	bill=conu7*8;
+	printf("%d",bill);
+	gotoxy(65,19);
+	printf("%d",bill);
+	gotoxy(37,20);
+	scanf("%d",&conu8);
+	gotoxy(52,20);
+	bill=conu8*8;
+	printf("%d",bill);
+	gotoxy(65,20);
+	printf("%d",bill);
+	gotoxy(37,21);
+	scanf("%d",&conu9);
+	gotoxy(52,21);
+	bill=conu9*8;
+	printf("%d",bill);
+	gotoxy(65,21);
+	printf("%d",bill);
+	gotoxy(37,22);
+	scanf("%d",&conu10);
+	gotoxy(52,22);
+	bill=conu10*8;
+	printf("%d",bill);
+	gotoxy(65,22);
+	printf("%d",bill);
+	gotoxy(37,23);
+	scanf("%d",&conu11);
+	gotoxy(52,23);
+	bill=conu11*8;
+	printf("%d",bill);
+	gotoxy(65,23);
+	printf("%d",bill);
+	gotoxy(37,24);
+	scanf("%d",&conu12);
+	gotoxy(52,24);
+	bill=conu12*8;
+	printf("%d",bill);
+	gotoxy(65,24);
+	printf("%d",bill);
+	gotoxy(-28,34);
+	scanf("%f",&rconu);
+	gotoxy(-28,36);
+	scanf("%f",&ce);
+	gotoxy(-28,40);
+	scanf("%f",&adj);
+	gotoxy(-28,42);
+	scanf("%f",&fulpr);
+	gotoxy(-28,52);
+	totall=ce+rconu+adj;
+	printf("%f",totall);
+	gotoxy(11,34);
+	scanf("%f",&Eldu);
+	gotoxy(11,36);
+	scanf("%f",&ptv);
+	gotoxy(11,44);
+	scanf("%f",&tax);
+	gotoxy(11,48);
+	scanf("%f",&sal);
+	gotoxy(11,52);
+	scanf("%f",&tr);
+	gotoxy(11,54);
+	scanf("%f",&taxonfp);
+	gotoxy(11,56);
+	toll=Eldu+ptv+tax+sal+tr+taxonfp;
+	printf("%f",toll);
+	gotoxy(60,34);
+	scanf("%f",&Arr);
+	gotoxy(60,36);
+	scanf("%d",&cbill);
+{
+
+	if(cbill<200)
+	chg=1.20;
+	else if(cbill>=200 && cbill<400)
+	chg=1.50;
+	else if(cbill>=400 && cbill<600)
+	chg=1.80;
+	else
+	chg=2.00;
+	calamt=cbill*chg;
+	if(calamt>=400)
+	surchage=(15*calamt)/100;
+	netamt=calamt+surchage;
+	gotoxy(60,42);
+	scanf("%d",&ext);
+	gotoxy(60,44);
+	tob=fulpr+taxonfp;
+	printf("%.2f",tob);
+	gotoxy(60,46);
+	printf("%.2f",calamt);
+	gotoxy(60,48);
+	printf("%.2f",surchage);
+	gotoxy(60,50);
+	printf("%.2f",netamt);	
+}	
+getch();
+}
